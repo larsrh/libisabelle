31cf79943194fe6f9c95214615a126e023f6f166
wneuper <wneuper@ist.tugraz.at>
2015-02-16 16:53:23 +0100

    completed operation ?; ERROR

--- examples/src/main/java/Hello_PIDE.java
+++ examples/src/main/java/Hello_PIDE.java
@@ -7,6 +7,7 @@ public class Hello_PIDE {
   public static void main(String args[]) {
     JSystem sys = JSystem.instance(new File("."), "Hello_PIDE");
     System.out.println(sys.sendCommand("hello", Arrays.asList("world")));
+    System.out.println(sys.sendCommand("Iterator", Arrays.asList(1)));
     sys.dispose();
   }
 
