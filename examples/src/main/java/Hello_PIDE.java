453dc9cc9f1acc0a2e00008d0c0633f5c1c1b17a
wneuper <wneuper@ist.tugraz.at>
2015-02-23 17:20:03 +0100

    made java output for trials clearer

--- examples/src/main/java/Hello_PIDE.java
+++ examples/src/main/java/Hello_PIDE.java
@@ -7,8 +7,11 @@ public class Hello_PIDE {
   public static void main(String args[]) {
     JSystem sys = JSystem.instance(new File("."), "Hello_PIDE");
     System.out.println(sys.sendCommand("hello", Arrays.asList("world")));
+    System.out.println("--- test_1:");
     System.out.println(sys.sendCommand("test_1", Arrays.asList(Integer.toString(111))));
+    System.out.println("--- test_2:");
     System.out.println(sys.sendCommand("test_2", Arrays.asList(Integer.toString(222))));
+    System.out.println("--- Iterator:");
     System.out.println(sys.sendCommand("Iterator", Arrays.asList(Integer.toString(1))));
     sys.dispose();
   }
