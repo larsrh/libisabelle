bb6ff6cdb684c9abdd45590d4b73b2dd6b831f58
Lars Hupel <lars.hupel@mytum.de>
2015-03-29 23:04:50 +0200

    rework codecs in ML; Scala part broken for now

---
+++ libisabelle/src/main/isabelle/Protocol/Codec_Test.thy
@@ -0,0 +1,30 @@
+theory Codec_Test
+imports Codec "~~/src/Tools/Spec_Check/Spec_Check"
+begin
+
+ML\<open>
+fun check_for str =
+  let
+    val prop =
+      "ALL x. (let val c = (" ^ str ^ ") in Codec.decode c (Codec.encode c x) = Codec.Success x end)"
+  in check_property prop end
+
+fun gen_unit r =
+  ((), r)
+\<close>
+
+ML_command\<open>
+  check_for "Codec.unit";
+  check_for "Codec.int";
+  check_for "Codec.string";
+  check_for "Codec.tuple Codec.int Codec.int";
+  check_for "Codec.tuple Codec.string Codec.unit";
+  check_for "Codec.list Codec.unit";
+  check_for "Codec.list Codec.int";
+  check_for "Codec.list Codec.string";
+  check_for "Codec.list (Codec.list Codec.string)";
+  check_for "Codec.list (Codec.tuple Codec.int Codec.int)";
+  check_for "Codec.tuple Codec.int (Codec.list Codec.int)";
+\<close>
+
+end
--- libisabelle/src/main/isabelle/Protocol/Protocol.thy
+++ libisabelle/src/main/isabelle/Protocol/Protocol.thy
@@ -1,45 +1,70 @@
 theory Protocol
-imports Pure
+imports Codec
+keywords "operation_setup" :: thy_decl % "ML"
 begin
 
 ML\<open>
-
 signature LIBISABELLE = sig
-  type operation = XML.body list -> XML.body
   type name = string
+  type ('i, 'o) operation =
+    {from_lib : 'i codec,
+     to_lib : 'o codec,
+     action : 'i -> 'o}
 
-  val add_operation : name -> operation -> unit
+  exception GENERIC of string
+
+  val add_operation : name -> ('i, 'o) operation -> unit
+  val operation_setup : bstring -> Symbol_Pos.source -> theory -> unit
 end
 
 structure Libisabelle : LIBISABELLE = struct
 
-type operation = XML.body list -> XML.body
 type name = string
+type ('i, 'o) operation =
+  {from_lib : 'i codec,
+   to_lib : 'o codec,
+   action : 'i -> 'o}
+
+type raw_operation = XML.tree -> XML.tree
+
+exception GENERIC of string
 
 val operations =
-  Synchronized.var "libisabelle.operations" (Symtab.empty: operation Symtab.table)
+  Synchronized.var "libisabelle.operations" (Symtab.empty: raw_operation Symtab.table)
 
-fun add_operation name operation =
-  Synchronized.change operations (Symtab.update_new (name, operation))
+fun add_operation name {from_lib, to_lib, action} =
+  let
+    fun raw tree =
+      case Codec.decode from_lib tree of
+        Codec.Success i => Codec.encode to_lib (action i)
+      | Codec.Failure (msg, _) => raise Fail ("decoding input failed " ^ msg)
+  in
+    Synchronized.change operations (Symtab.update (name, raw))
+  end
 
-
-val encode_result = XML.Encode.variant
-  [fn Exn.Res a => ([], a),
-   fn Exn.Exn exn => ([], XML.Encode.string (@{make_string} exn))]
+val result_codec =
+  let
+    (* slightly fishy codec, doesn't preserve exception type *)
+    fun enc (Exn.Res tree) = (0, Codec.encode Codec.tree tree)
+      | enc (Exn.Exn exn) = (1, Codec.encode Codec.string (@{make_string} exn))
+    fun dec 0 = SOME (Codec.decode Codec.tree #> Codec.map_result Exn.Res)
+      | dec 1 = SOME (Codec.decode Codec.string #> Codec.map_result (Exn.Exn o GENERIC))
+      | dec _ = NONE
+  in Codec.variant enc dec "Exn.Result" end
 
 val _ = Isabelle_Process.protocol_command "libisabelle"
-  (fn id :: name :: args =>
+  (fn id :: name :: [arg] =>
     let
       val id = Markup.parse_int id
       val response =
         [(Markup.functionN, "libisabelle_response"),
          ("id", Markup.print_int id)]
-      val args = map YXML.parse_body args
+      val args = YXML.parse arg
       fun exec f =
         (Future.fork (fn () =>
           let
             val res = Exn.interruptible_capture f args
-            val yxml = YXML.string_of_body (encode_result res)
+            val yxml = YXML.string_of (Codec.encode result_codec res)
           in
             Output.protocol_message response [yxml]
           end);
@@ -50,18 +75,22 @@ val _ = Isabelle_Process.protocol_command "libisabelle"
       | NONE => exec (fn _ => raise Fail "libisabelle: unknown command"))
     end)
 
-val _ = add_operation "hello"
-  (fn [raw_data] =>
-    let
-      val data = XML.Decode.string raw_data
-      val result = "Hello " ^ data
-    in
-      XML.Encode.string result
-    end
-  )
+fun operation_setup name source thy =
+  ML_Context.eval_in (SOME (Proof_Context.init_global thy)) ML_Compiler.flags (#pos source)
+    (ML_Lex.read Position.none ("Libisabelle.add_operation " ^ ML_Syntax.print_string name ^ "(") @
+      ML_Lex.read_source false source @
+      ML_Lex.read Position.none ")")
 
 end
 
+val _ = Outer_Syntax.command @{command_spec "operation_setup"} "define protocol operation in ML"
+  (Parse.name -- Parse.!!! (@{keyword "="} |-- Parse.ML_source)
+    >> (fn (name, txt) => Toplevel.theory (tap (Libisabelle.operation_setup name txt))))
 \<close>
+
+operation_setup hello = \<open>
+  {from_lib = Codec.string,
+   to_lib = Codec.string,
+   action = (fn data => "Hello " ^ data)}\<close>
 
 end
