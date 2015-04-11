31e5e64e83414b724bd2f88bab82b0a35e043003
Lars Hupel <lars.hupel@mytum.de>
2015-04-12 00:18:42 +0200

    tests for loading theories

--- libisabelle/src/test/scala/LibisabelleSpec.scala
+++ libisabelle/src/test/scala/LibisabelleSpec.scala
@@ -13,20 +13,25 @@ class LibisabelleSpec extends Specification with NoTimeConversions { def is = s2
   Basic protocol interaction
 
   An Isabelle session
-    can be started        $start
-    reacts to requests    $req
-    can be torn down      $stop"""
+    can be started          $start
+    can load theories       $load
+    reacts to requests      $req
+    can be torn down        $stop"""
 
+
+  val TypeOf = Operation.implicitly[String, String]("type_of")
 
   val system = System.instance(Some(new java.io.File(".")), "Protocol")
-  val response = system.flatMap(_.invoke(Operation.Hello)("world"))
+  val loaded = system.flatMap(_.loadTheories(new java.io.File("libisabelle/src/test/isabelle/Test")))
+  val response = for { s <- system; _ <- loaded; res <- s.invoke(TypeOf)("op ==>") } yield res
   val teardown = for { s <- system; _ <- response /* wait for response */; _ <- s.dispose } yield ()
 
   def exist[A]: Matcher[A] = ((a: A) => a != null, "doesn't exist")
 
 
   def start = system must exist.await(timeout = 30.seconds)
-  def req = response must beRight("Hello world").await(timeout = 30.seconds)
+  def load = loaded must beTrue.await(timeout = 30.seconds)
+  def req = response must beRight("prop => prop => prop").await(timeout = 30.seconds)
   def stop = teardown must exist.await(timeout = 30.seconds)
 
 }
