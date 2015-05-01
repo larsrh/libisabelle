21390e09de3dfc13fa56d402098415b3bf6f7d4d
Lars Hupel <lars.hupel@mytum.de>
2015-05-01 13:09:51 +0200

    bracketed execution enables feeding of all messages

--- libisabelle/src/main/scala/System.scala
+++ libisabelle/src/main/scala/System.scala
@@ -15,20 +15,6 @@ object System {
   }
 
 
-  // implementation details
-
-  private val ID = "id"
-  private val LIBISABELLE_RESPONSE = "libisabelle_response"
-
-  private object Libisabelle_Response {
-    def unapply(props: Properties.T): Option[Long] = props match {
-      case
-        (Markup.FUNCTION, LIBISABELLE_RESPONSE) ::
-        (ID, Properties.Value.Long(id)) :: Nil => Some(id)
-      case _ => None
-    }
-  }
-
   private def mkPhaseListener(session: Session, phase: Session.Phase)(implicit ec: ExecutionContext): Future[Unit] = {
     val promise = Promise[Unit]
     val consumer = Session.Consumer[Session.Phase]("phase-listener") {
@@ -65,15 +51,38 @@ object System {
   }
 
 
+  private class Output(name: String) {
+    def unapply(props: Properties.T): Option[Long] = props match {
+      case
+        (Markup.FUNCTION, `name`) ::
+        ("id", Properties.Value.Long(id)) :: Nil => Some(id)
+      case _ => None
+    }
+  }
+
+  private object Output {
+    object Response extends Output("libisabelle_response")
+    object Start extends Output("libisabelle_start")
+    object Stop extends Output("libisabelle_stop")
+  }
+
+
   private trait OperationState { self =>
     type T
-    val bracketed: Boolean
+    val firehose: Boolean
     val observer: Observer[T]
     val promise: Promise[Exn.Result[T]]
 
+    def withFirehose(firehose0: Boolean) = new OperationState {
+      type T = self.T
+      val firehose = firehose0
+      val observer = self.observer
+      val promise = self.promise
+    }
+
     def withObserver(observer0: Observer[T]) = new OperationState {
       type T = self.T
-      val bracketed = self.bracketed
+      val firehose = self.firehose
       val observer = observer0
       val promise = self.promise
     }
@@ -88,12 +97,19 @@ object System {
       case Observer.More(step, finish) => msg match {
         case msg: Prover.Protocol_Output =>
           msg.properties match {
-            case System.Libisabelle_Response(id1) if id == id1 =>
-              val xml = YXML.parse(msg.text)
-              withObserver(finish(xml))
+            case Output.Response(id1) if id == id1 =>
+              withObserver(finish(YXML.parse(msg.text)))
+            case Output.Start(id1) if id == id1 && !firehose =>
+              withFirehose(true)
+            case Output.Stop(id1) if id == id1 && firehose =>
+              withFirehose(false)
+            case _ if firehose =>
+              withObserver(step(msg))
             case _ =>
               this
           }
+        case _ if firehose =>
+          withObserver(step(msg))
         case _ =>
           this
       }
@@ -106,7 +122,7 @@ object System {
   private def mkOperationState[T0](observer0: Observer[T0]) = {
     val state = new OperationState {
       type T = T0
-      val bracketed = false
+      val firehose = false
       val observer = observer0
       val promise = Promise[Exn.Result[T]]
     }
