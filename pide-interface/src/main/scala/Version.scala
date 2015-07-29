98e2d18ead5701bc0f9ab35bd35ca21bc68abb7a
Lars Hupel <lars.hupel@mytum.de>
2015-07-29 13:10:49 +0200

    no more default version

--- pide-interface/src/main/scala/Version.scala
+++ pide-interface/src/main/scala/Version.scala
@@ -2,13 +2,6 @@ package edu.tum.cs.isabelle.api
 
 import java.nio.file.Path
 
-object Version {
-  val Isa2014 = Version("2014")
-  val Isa2015 = Version("2015")
-
-  val latest = Isa2015
-}
-
 case class Version(identifier: String) {
   final override def toString: String =
     s"<Isabelle$identifier>"
--- setup/src/main/scala/Setup.scala
+++ setup/src/main/scala/Setup.scala
@@ -4,12 +4,10 @@ import java.nio.file.{Files, Path, Paths}
 
 import scala.concurrent.{Future, ExecutionContext}
 
+import edu.tum.cs.isabelle.Implementations
 import edu.tum.cs.isabelle.api.{Environment, Version}
 
 object Setup {
-
-  def defaultVersion =
-    Version.latest
 
   def defaultBasePath =
     Paths.get("contrib")
@@ -17,27 +15,19 @@ object Setup {
   def defaultPlatform =
     Platform.guess
 
-  private def defaultInstallTo(path: Path)(implicit ec: ExecutionContext) =
-    defaultPlatform.flatMap(_.url(defaultVersion)) match {
+  private def defaultInstallTo(path: Path, version: Version)(implicit ec: ExecutionContext) =
+    defaultPlatform.flatMap(_.url(version)) match {
       case None =>
         sys.error("couldn't determine URL")
       case Some(url) =>
         val stream = Tar.download(url)
-        Tar.extractTo(path, stream).map(Setup(_, defaultVersion))
+        Tar.extractTo(path, stream).map(Setup(_, version))
     }
 
-  def defaultSetup(implicit ec: ExecutionContext): Future[Setup] =
-    detectSetup(defaultVersion, defaultBasePath) match {
-      case Some(install) =>
-        Future.successful(install)
-      case None =>
-        defaultInstallTo(defaultBasePath)
-    }
+  def temporarySetup(version: Version)(implicit ec: ExecutionContext): Future[Setup] =
+    defaultInstallTo(Files.createTempDirectory("libisabelle").toRealPath(), version)
 
-  def temporarySetup(implicit ec: ExecutionContext): Future[Setup] =
-    defaultInstallTo(Files.createTempDirectory("libisabelle").toRealPath())
-
-  def detectSetup(version: Version, base: Path): Option[Setup] = {
+  def detectSetup(base: Path, version: Version): Option[Setup] = {
     val path = base resolve s"Isabelle${version.identifier}"
     if (Files.isDirectory(path))
       Some(Setup(path, version))
@@ -45,6 +35,18 @@ object Setup {
       None
   }
 
+  def defaultSetup(version: Version)(implicit ec: ExecutionContext): Future[Setup] =
+    detectSetup(defaultBasePath, version) match {
+      case Some(install) =>
+        Future.successful(install)
+      case None =>
+        defaultInstallTo(defaultBasePath, version)
+    }
+
+
 }
 
-case class Setup(home: Path, version: Version)
+case class Setup(home: Path, version: Version) {
+  def makeEnvironment(impls: Implementations): Option[Environment] =
+    impls.makeEnvironment(home, version)
+}
