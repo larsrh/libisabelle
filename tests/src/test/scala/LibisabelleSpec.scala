4b621a1b5896e5a2367ef21e9be1110c9d624f5d
Lars Hupel <lars.hupel@mytum.de>
2015-07-30 09:46:13 +0200

    test for failing operations

--- tests/src/test/scala/LibisabelleSpec.scala
+++ tests/src/test/scala/LibisabelleSpec.scala
@@ -1,5 +1,6 @@
 package edu.tum.cs.isabelle.tests
 
+import scala.concurrent._
 import scala.concurrent.duration._
 
 import org.specs2.Specification
@@ -16,8 +17,9 @@ class LibisabelleSpec(val specs2Env: Env) extends Specification with DefaultSetu
   An Isabelle session
     can be started          ${system must exist.awaitFor(30.seconds)}
     can load theories       ${loaded must beRight(()).awaitFor(30.seconds)}
-    reacts to requests      ${response must beRight("prop => prop => prop").awaitFor(30.seconds)}
-    can be torn down        ${teardown must exist.awaitFor(30.seconds)}"""
+    reacts to requests      ${response must beRight("prop => prop => prop").awaitFor(5.seconds)}
+    handles errors          ${error must beLeft.awaitFor(5.seconds)}
+    can be torn down        ${teardown must exist.awaitFor(5.seconds)}"""
 
 
   implicit val ee = specs2Env.executionEnv
@@ -27,7 +29,11 @@ class LibisabelleSpec(val specs2Env: Env) extends Specification with DefaultSetu
   val system = System.create(env)(config)
   val loaded = system.flatMap(_.invoke(Operation.UseThys)(List("tests/src/test/isabelle/Test")))
   val response = for { s <- system; _ <- loaded; res <- s.invoke(TypeOf)("op ==>") } yield res
-  val teardown = for { s <- system; _ <- response /* wait for response */; _ <- s.dispose } yield ()
+  val error = for { s <- system; _ <- loaded; res <- s.invoke(TypeOf)("==>") } yield res
+
+  val responses = Future.sequence(List(response, error))
+
+  val teardown = for { s <- system; _ <- responses; _ <- s.dispose } yield ()
 
   def exist[A]: Matcher[A] = ((a: A) => a != null, "doesn't exist")
 
