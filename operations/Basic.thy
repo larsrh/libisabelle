8c178555bc08d4317959b6709f2c1066b0122ea7
Lars Hupel <lars.hupel@mytum.de>
2016-01-09 23:15:12 +0100

    don't send back un-checked terms

--- operations/Basic.thy
+++ operations/Basic.thy
@@ -12,28 +12,23 @@ operation_setup (sequential, bracket) use_thys = \<open>
    to_lib = Codec.unit,
    action = Thy_Info.use_thys o map (rpair Position.none)}\<close>
 
-ML\<open>
-(* FIXME is this somewhere in the library? Term_Position.strip_positions doesn't do it *)
-fun strip_positions ((t as Const ("_type_constraint_", Type ("fun", [t1, _]))) $ u) =
-      if is_some (Term_Position.decode_positionT t1) then
-        strip_positions u
-      else
-        t $ strip_positions u
-  | strip_positions (t $ u) = strip_positions t $ strip_positions u
-  | strip_positions (Abs (x, T, t)) = Abs (x, T, strip_positions t)
-  | strip_positions t = t
+text \<open>
+  The @{verbatim read_term} operation performs both parsing and checking at once, because we do not
+  want to send back parsed, but un-checked terms to the JVM. They may contain weird position
+  information which are difficult to get rid of and confuse the codecs.
 \<close>
 
-operation_setup parse_term = \<open>
-  {from_lib = Codec.tuple Codec.string Codec.string,
+operation_setup read_term = \<open>
+  {from_lib = Codec.triple Codec.string Codec.typ Codec.string,
    to_lib = Codec.option Codec.term,
-   action = (fn (raw_term, thy_name) =>
+   action = (fn (raw_term, typ, thy_name) =>
     let
       val thy = Thy_Info.get_theory thy_name
       val ctxt = Proof_Context.init_global thy
     in
       try (Syntax.parse_term ctxt) raw_term
-      |> Option.map strip_positions
+      |> Option.map (Type.constraint typ)
+      |> Option.mapPartial (try (Syntax.check_term ctxt))
     end)}\<close>
 
 operation_setup check_term = \<open>
