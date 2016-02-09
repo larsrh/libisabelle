f186d7a1793b5536b72ffa9b178efe6deda8d4eb
Lars Hupel <lars.hupel@mytum.de>
2016-02-09 23:06:14 +0100

    extract position information from prover markup

--- cli/src/main/scala/Report.scala
+++ cli/src/main/scala/Report.scala
@@ -9,17 +9,38 @@ import edu.tum.cs.isabelle.api._
 
 object Report extends Command {
 
+  sealed abstract class Format
+  case object RawXML extends Format
+  case object XRay extends Format
+
+  def print(reports: Reports, home: Path, format: Format): Unit = format match {
+    case RawXML =>
+      println(s"<?xml version='1.0' ?>\n<dump home='$home'>\n")
+      reports.items.foreach(tree => println(tree.pretty(2)))
+      println("\n</dump>")
+    case XRay =>
+      println(reports.interpret(home).pretty)
+  }
+
   def run(bundle: Bundle, args: List[String])(implicit ec: ExecutionContext): Future[Unit] = {
-    def markup(tree: XML.Tree) = println(tree.pretty)
-    def finish() = println("\n</dump>")
+    val (format, files) = args match {
+      case "--format" :: "raw-xml" :: files => (RawXML, files)
+      case "--format" :: "x-ray" :: files => (XRay, files)
+      case "--format" :: _ => sys.error("unknown format")
+      case _ => (RawXML, args)
+    }
+
+    var reports = Reports.empty
+
+    def markup(tree: XML.Tree) = reports += tree
+    def finish() = ()
 
     for {
       s <- System.create(bundle.env, bundle.configuration)
-      _ = println(s"<?xml version='1.0' ?>\n<dump home='${bundle.env.home}'>\n")
-      _ <- s.invoke(Operation.UseThys(markup, finish))(args)
+      _ <- s.invoke(Operation.UseThys(markup, finish))(files)
       _ <- s.dispose
     }
-    yield ()
+    yield print(reports, bundle.env.home, format)
   }
 
 }
