395c9c0938b83a4532b57e665447d0d8484273a2
Lars Hupel <lars.hupel@mytum.de>
2016-04-22 19:48:29 +0200

    make errors more explicit

--- setup/src/main/scala/japi/JSetup.scala
+++ setup/src/main/scala/japi/JSetup.scala
@@ -6,13 +6,20 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 
+import cats.data.Xor
+
 import edu.tum.cs.isabelle.api._
 import edu.tum.cs.isabelle.setup._
 
 object JSetup {
 
   def defaultSetup(version: Version, timeout: Duration): Setup =
-    Await.result(Setup.defaultSetup(version), timeout)
+    Setup.defaultSetup(version) match {
+      case Xor.Right(future) =>
+        Await.result(future, timeout)
+      case Xor.Left(reason) =>
+        sys.error(reason.explain)
+    }
 
   def defaultSetup(version: Version): Setup =
     defaultSetup(version, Duration.Inf)
