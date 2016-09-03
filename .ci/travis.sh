ce3e9aa6a8ba97e6bcb6bdf2be9dd3c13caaeadf
Lars Hupel <lars.hupel@mytum.de>
2016-09-03 14:11:05 +0200

    tuned

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -4,11 +4,9 @@ set -x
 
 ./sbt "++$SCALA_VERSION" compile
 ./sbt "++$SCALA_VERSION" "cli/run --version $ISABELLE_VERSION --session Protocol build"
+./sbt "++$SCALA_VERSION" validateQuick
 
 if [ "$TRAVIS_OS" = "linux" ]; then
   ./sbt "++$SCALA_VERSION" "cli/run --version $ISABELLE_VERSION --session HOL-Protocol build"
-  ./sbt "++$SCALA_VERSION" validateQuick
   ./sbt "++$SCALA_VERSION" validateSlow
-else
-  ./sbt "++$SCALA_VERSION" validateQuick
 fi
