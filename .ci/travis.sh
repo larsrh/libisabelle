61adc6c4ab6ef2d16b3b67009416d688e4762b1b
Lars Hupel <lars.hupel@mytum.de>
2016-09-04 12:35:51 +0200

    check Scaladoc in CI so that it doesn't happen again

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -9,4 +9,6 @@ set -x
 if [ "$TRAVIS_OS" = "linux" ]; then
   ./sbt "++$SCALA_VERSION" "cli/run --version $ISABELLE_VERSION --session HOL-Protocol build"
   ./sbt "++$SCALA_VERSION" validateSlow
+else
+  ./sbt "++$SCALA_VERSION" publishLocal
 fi
