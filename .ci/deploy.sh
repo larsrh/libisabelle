e052d261bf078c8421757dab2beb9ee996656275
Lars Hupel <lars.hupel@mytum.de>
2016-09-15 22:25:04 +0200

    deploy CLI JAR to Bintray

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-  echo "This commit was made against the '$TRAVIS_BRANCH' branch and not 'master', no deploy."
-  exit 0
-fi
+set -x
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "This commit is in a pull request, no deploy."
-  exit 0
-fi
-
-if [ "$PROFILE" = "slow" ]; then
-  git config --global user.name "Lars Hupel"
-  git config --global user.email "lars.hupel@mytum.de"
-  ./sbt "++$SCALA_VERSION" pushSite
-else
-  echo "This build job executes the '$PROFILE' profile and not 'slow', no deploy."
-fi
+git config --global user.name "Lars Hupel"
+git config --global user.email "lars.hupel@mytum.de"
+./sbt "++$SCALA_VERSION" pushSite
--- .ci/travis.sh
+++ .ci/travis.sh
@@ -9,6 +9,7 @@ set -x
 if [ "$PROFILE" = "slow" ]; then
   ./sbt "++$SCALA_VERSION" "cli/run --version $ISABELLE_VERSION --session HOL-Protocol build"
   ./sbt "++$SCALA_VERSION" validateSlow
+  ./sbt "++$SCALA_VERSION" "cli/assembly"
 else
   ./sbt "++$SCALA_VERSION" publishLocal
 fi
