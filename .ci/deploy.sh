d6da6dbdf74c360fb8aafb8b949e9cfda76d9cd8
Lars Hupel <lars.hupel@mytum.de>
2016-09-15 18:34:38 +0200

    let Travis deploy to GitHub pages on push

---
+++ .ci/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  echo "This commit was made against the '$TRAVIS_BRANCH' branch and not 'master', no deploy."
+  exit 0
+fi
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "This commit is in a pull request, no deploy."
+  exit 0
+fi
+
+if [ "$PROFILE" = "slow" ]; then
+  git config user.name "Lars Hupel"
+  git config user.email "lars.hupel@mytum.de"
+  ./sbt "++$SCALA_VERSION" pushSite
+else
+  echo "This build job executes the '$PROFILE' profile and not 'slow', no deploy."
+fi
