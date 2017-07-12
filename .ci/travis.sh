70435b92b0634804966ca643d19c302ede8070f3
Lars Hupel <lars.hupel@mytum.de>
2017-07-12 21:16:03 +0200

    make 'stable' variant explicit in Travis config

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -6,11 +6,11 @@ curl -s https://raw.githubusercontent.com/paulp/sbt-extras/master/sbt > sbt
 chmod 0755 sbt
 
 ./sbt "++$SCALA_VERSION" compile
-./sbt "++$SCALA_VERSION" "cli/run --version $ISABELLE_VERSION --session Protocol --internal --verbose build"
+./sbt "++$SCALA_VERSION" "cli/run --version stable:$ISABELLE_VERSION --session Protocol --internal --verbose build"
 ./sbt "++$SCALA_VERSION" validateQuick
 
 if [ "$PROFILE" = "slow" ]; then
-  ./sbt "++$SCALA_VERSION" "cli/run --version $ISABELLE_VERSION --session HOL-Protocol --internal --verbose build"
+  ./sbt "++$SCALA_VERSION" "cli/run --version stable:$ISABELLE_VERSION --session HOL-Protocol --internal --verbose build"
   ./sbt "++$SCALA_VERSION" validateSlow
   ./sbt "++$SCALA_VERSION" "cli/assembly"
   if [ "$DEPLOY" = "1" ]; then
